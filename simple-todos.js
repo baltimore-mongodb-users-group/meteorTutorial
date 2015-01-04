dc2b41c61d8892f95ec27f7acf4bf00c99bf6275
Douglas Paul <douglas.w.paul@gmail.com>
2015-01-04 14:23:11 -0500

    Leverage login functionality (tutorial step 9, part 2)

--- simple-todos.js
+++ simple-todos.js
@@ -35,7 +35,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user // current time
       });
 
       // Clear form
